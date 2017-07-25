c32e7bff86cf9471ac44d7c0f4d1e2435fd9d409
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-07-25 14:18:21 -0400

    submit sends data to server

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-
+var URL = "http://dc-coffeerun.herokuapp.com/api/coffeeorders"
 var $form = $("[data-coffee-order='form']")
 
 
@@ -13,17 +13,21 @@ var $strength = $("#strengthLevel");
 $form.on("submit", function(event){
 	event.preventDefault();
 	var $radios = $('input[type="radio"]:checked');
-	var store = makeObject($coffeeOrder.val(), $email.val());
+	var store = makeObject($coffeeOrder.val(), $email.val(), $radios.val(), $flavorShot.val(), $strength.val());
 	var dataString = JSON.stringify(store);
 	localStorage.setItem( "Coffee Orders", dataString);
+	sendDataToServer(URL, store);
 
 });
 
-function makeObject(coffeeOrder, email){
+function makeObject(coffeeOrder, email, radio, flavor, strength){
 	var object = {};
 
 	object["emailAddress"] = email;
 	object["coffee"] = coffeeOrder; 
+	object["size"] = radio;
+	object["flavor"] = flavor;
+	object["strength"] = strength;
 
 
 	return object;
@@ -39,16 +43,26 @@ function getObject(){
 	
 }
 
-getObject()
+
+
+
+
+function getServerData(site){
+	$.get(site, function(x){
+		console.log(x)
+	})
+}
+
+function sendDataToServer(site, object){
+	$.post(site, object, function(x){
+		console.log(x);
+	})
+}
 
 
 
 
 
-// ajax-JSON-API's 
-
-// json.parse
 
 
-
-// json.stringify
+// dropdown 
